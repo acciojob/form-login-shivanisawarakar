cad41394ebdad3857b32978945b8dc9559930f32
shivani sawarkar <117519427+shivanisawarakar@users.noreply.github.com>
2023-05-02 16:08:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	let firstName = document.querySelector('input[name = fname').value;
+	let lastName = document.querySelector('input[name = lname').value;
+	alert(firstName + " " + lastName);
 
 }
